834b41e65d8d32fcc90eb9f3365a67819b671d8a
Deepak Garg <garg882001@gmail.com>
2022-10-11 14:33:00 +0530

    move to ES6

--- server/app.js
+++ server/app.js
@@ -1,14 +1,24 @@
-const express = require("express");
-const dotENV = require("dotenv");
-const mongoose = require("mongoose");
-const https = require('https');
+import express from "express";
+import dotENV from "dotenv";
+import mongoose from "mongoose";
+import https from "https";
 
 const app = express();
 
 //  Importing dotENV file
 dotENV.config({ path: "./config.env" });
 
-require("./DataBase/DB_Excess");
+
+const URL = process.env.DB_URL;
+
+  mongoose.connect(URL,{useNewUrlParser : true}).then(()=>{
+    console.log(" MongoDb Connected to Server Successfully !");
+  }
+  ).catch((error)=>{
+         console.log(error);
+  });
+
+
 
 // Starting server at PORT No.
 const PORT = process.env.PORT;
