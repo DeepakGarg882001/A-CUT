793664526ad6f447882f029929eb66c8b632ded3
JOGINDER571 <snshrma7@gmail.com>
2022-11-12 17:13:04 +0530

    navbar,home,router updated

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,9 +1,41 @@
 import React from 'react'
-
+import "../../styles/home.css";
+import video from "../../Assets/video.mp4";
+import s1 from "../../Assets/home1.jpg";
+import s2 from "../../Assets/home2.jpg";
+import s3 from "../../Assets/home3.jpg";
 const Home = () => {
   return (
     <>
-    <h1>hello</h1>
+    <div>
+      <div className="Work-flow">
+        <h2>Total work flow of this app by seing below video</h2>
+        <video controls>
+        <source src={video} type="video/mp4"/>
+        Your browser does not support the video tag.
+        </video>
+      </div>
+      <div className="shops">
+        <h2>Available Shops</h2>
+        <div className='individual'>
+          <div className="individual-shop">
+            <h3>shop1</h3>
+            <img src={s1} alt="img" />
+            <button className='btn'>Book Now</button>
+          </div>
+          <div className="individual-shop">
+            <h3>shop2</h3>
+            <img src={s2} alt="img" />
+            <button className='btn'>Book Now</button>
+          </div>
+          <div className="individual-shop">
+            <h3>shop3</h3>
+            <img src={s3} alt="img" />
+            <button className='btn'>Book Now</button>
+          </div>
+          </div>
+      </div>
+    </div>
     </>
   )
 }
---
+++ client/src/components/shop/Shop1.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "../../styles/shop1.css";
+import img1 from "../../Assets/shop1.jpg";
+import img2 from "../../Assets/shop2.jpg";
+import img3 from "../../Assets/shop3.jpg";
+const Shop1 = () => {
+  return (
+    <>
+      <header>
+        <h2>Near Government School (Jant-Pali Mahendergarh)</h2>
+        <div className="designer">
+          <h3>Designs you can select here...</h3>
+          <div className="designs">
+            <div className="design">
+              <img src={img1} alt="img" />
+            </div>
+            <div className="design">
+              <img src={img2} alt="img" />
+            </div>
+            <div className="design">
+              <img src={img3} alt="img" />
+            </div>
+          </div>
+          <div className="designs">
+            <div className="design">
+              <img src={img1} alt="img" />
+            </div>
+            <div className="design">
+              <img src={img2} alt="img" />
+            </div>
+            <div className="design">
+              <img src={img3} alt="img" />
+            </div>
+          </div>
+          <h3>Enter the details for booking </h3>
+          <form id="form">
+            <input type="text" id="name" placeholder="Enter your name" />
+            <select name="service" id="hair-cut">
+              <option value="hair-cut">Select services you want</option>
+              <option value="hair-cut">hair-cut</option>
+              <option value="Beared">Beared</option>
+              <option value="head-massage">head-massage</option>
+              <option value="hair-color">hair-color</option>
+            </select>
+            <button className="btn">Submit</button>
+          </form>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Shop1;
