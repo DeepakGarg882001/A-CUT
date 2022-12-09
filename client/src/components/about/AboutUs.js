10d5d2335fdfcfb46d73f59186bc39f901f64461
Jitendra kumar <jitendrakumar8080397415@gmail.com>
2022-12-09 15:41:30 +0530

    choose account pages created

--- client/src/components/about/AboutUs.js
+++ client/src/components/about/AboutUs.js
@@ -10,6 +10,7 @@ const AboutUs = () => {
           <img src={img1} alt="loading" />
         </div>
         <div className="about-content">
+          <p className="about_main">ABOUT OUR APP</p>
           <p>Our motivation for making this app is to save the time.
             Whenever we are going in a saloon for the hair_cut ,In most 
             of the time the croud of 5 to 6 people are sitting there and
@@ -25,9 +26,9 @@ const AboutUs = () => {
       <p id="team_heading">Team Member</p>
 
         <div className="team_inner">
-          <div className="team_member">JITENDRA KUMAR </div>
-          <div className="team_member">DEEPAK GARG</div>
-          <div className="team_member">JOGINDER SHARMA</div>
+          <div className="team_member" id="jitu_details">JITENDRA KUMAR </div>
+          <div className="team_member" id ="garg_details">DEEPAK GARG</div>
+          <div className="team_member" id="sharma_details">JOGINDER SHARMA</div>
         </div>
 
       </div>
