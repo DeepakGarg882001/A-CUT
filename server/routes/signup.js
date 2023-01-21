afa2a03a51cccfe67cc07ceab0e2723d2f35d9cf
Jitendra kumar <jitendrakumar8080397415@gmail.com>
2023-01-21 13:14:37 +0530

    message changed

--- server/routes/signup.js
+++ server/routes/signup.js
@@ -11,7 +11,7 @@ export const signup = async (request, response) => {
   try {
     const { name, email, phone, password } = request.body;
 
-    if (!name || !email || !phone || !password ) {
+    if (!name || !email || !phone || !password) {
       return response
         .status(401)
         .json({ error: "Please fill the form completely !" });
@@ -28,9 +28,9 @@ export const signup = async (request, response) => {
     const salt = bcrypt.genSaltSync();
     const secure = await bcrypt.hash(password, salt);
 
-    const userDetails = { name, email, phone, password:secure };
+    const userDetails = { name, email, phone, password: secure };
 
-    const addUser = await UserCol.updateMany({email},userDetails);
+    const addUser = await UserCol.updateMany({ email }, userDetails);
 
     if (addUser) {
       // Send a Mail to user
@@ -50,8 +50,7 @@ export const signup = async (request, response) => {
         from: "salondekhoo@gmail.com",
         to: email,
         subject: " Salon Dekho Registration ",
-        text: `Hi ${name}, You have successfuly Created an account at Salon Dekho .
-                  Thankyou   `,
+        text: `Hi ${name}, "Welcome to our website! Thank you for registered at Salon Dekho. Your account is now active and you can start enjoying our services. Please save your login details for future reference. If you have any queries or issues, please contact our customer support. Thanks for choosing us."  `,
       };
       transporter.sendMail(mailMessage, async (error, info) => {
         if (error) {
@@ -62,7 +61,7 @@ export const signup = async (request, response) => {
             .json({ message: "You have Successfully Registered !" });
         }
       });
-      
+
     } else {
       response.status(401).json({ error: " Register Process Failed !" });
     }
@@ -72,60 +71,60 @@ export const signup = async (request, response) => {
 };
 
 // Send a OTP to User Email to verify the email address
-export const sendOTPtoEmail = async(request,response)=>{
-  
-    const {role,email} = request.body;
+export const sendOTPtoEmail = async (request, response) => {
 
-    if(!role|| !email){
-      return response.status(400).json({error:"Provide Proper Details"})
+  const { role, email } = request.body;
+
+  if (!role || !email) {
+    return response.status(400).json({ error: "Provide Proper Details" })
+  }
+
+  const checkEmail = await UserCol.findOne({ email });
+  if (checkEmail) {
+    return response.status(200).json({ error: "Account Already Exist. Please Login !" });
+  }
+  const otp = Math.floor(1000 + Math.random() * 9000);
+
+  const data = {
+    OTP: otp + 1,
+    role: role,
+    email: email,
+  };
+  const emailVerifyToken = jwt.sign(data, Secure_Key, { expiresIn: "0.5h" });
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    requireTLS: true,
+    auth: {
+      user: "salondekhoo@gmail.com",
+      pass: "vbgjcawbzbithelw",
+    },
+  });
+
+  const mailMessage = {
+    from: "salondekhoo@gmail.com",
+    to: email,
+    subject: " Email Verification ",
+    text: ` Hi, the secret OTP code is : ${otp} . Please never share with others.  `,
+  };
+  transporter.sendMail(mailMessage, async (error, info) => {
+    if (error) {
+      return response.status(500).json({ error: "OTP Sending Process failed Due to Very Poor Internet Connection,Try Again" });
+    } else {
+      response
+        .status(201)
+        .cookie("A_CUT_Email", emailVerifyToken)
+        .json({ token: emailVerifyToken });
     }
-    
-    const checkEmail = await UserCol.findOne({email});
-    if(checkEmail){
-      return response.status(200).json({error:"Account Already Exist. Please Login !"});
-    }
-    const otp = Math.floor(1000 + Math.random() * 9000);
+  });
 
-    const data = {
-      OTP: otp + 1,
-      role:role,
-      email:email,
-    };
-    const emailVerifyToken = jwt.sign(data, Secure_Key, { expiresIn: "0.5h" });
-
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      requireTLS: true,
-      auth: {
-        user: "salondekhoo@gmail.com",
-        pass: "vbgjcawbzbithelw",
-      },
-    });
-
-    const mailMessage = {
-      from: "salondekhoo@gmail.com",
-      to: email,
-      subject: " Email Verification ",
-      text: ` Hi, the secret OTP code is : ${otp} . Please never share with others.  `,
-    };
-    transporter.sendMail(mailMessage, async (error, info) => {
-      if (error) {
-        return response.status(500).json({ error:"OTP Sending Process failed Due to Very Poor Internet Connection,Try Again" });
-      } else {
-        response
-          .status(201)
-          .cookie("A_CUT_Email", emailVerifyToken)
-          .json({ token: emailVerifyToken });
-      }
-    });
-    
 }
 
 
 // Verify the OTP entered by the user to verify its email
-export const verifyOTPforEmail = async(request ,response)=>{
+export const verifyOTPforEmail = async (request, response) => {
 
   const { userOtp } = request.body;
 
@@ -146,13 +145,13 @@ export const verifyOTPforEmail = async(request ,response)=>{
   console.log(email);
   console.log(role);
   console.log(userOtp);
-  
-  if(Number(userOtp) === otp) {
-    await UserCol.create({email,userRole:{role}});
 
-    response.status(201).json({ message: "OTP Matched!" ,email});
+  if (Number(userOtp) === otp) {
+    await UserCol.create({ email, userRole: { role } });
+
+    response.status(201).json({ message: "OTP Matched!", email });
   } else {
-    
+
     return response.status(401).json({ error: " Invalid OTP !" });
   }
 }
